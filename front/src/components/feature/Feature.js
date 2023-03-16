4181a4f400223ce5155996fd1b111867fe1ae3d3
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-03-16 13:40:19 -0400

    update

--- front/src/components/feature/Feature.js
+++ front/src/components/feature/Feature.js
@@ -1,24 +1,63 @@
 import React from 'react'
 import "./feature.css"
-import Carousel from 'nuka-carousel';
-
+import ReactBootstrapCarousel from "react-bootstrap-carousel";
+import "bootstrap/dist/css/bootstrap.css";
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+import Carousel from 'react-bootstrap/Carousel';
+import { IconButton, Box, Typography, useTheme,  Button } from "@mui/material";
 
 
 const Feature = () => {
 
-  const params = {
-    wrapAround: true,
-    animation: 'zoom',
-    slidesToShow: 2,
-  }
   return (
-<div>
-<Carousel {...params}>
+<Box
+maxWidth={600}
+margin="auto"
+marginTop={6}
+maxHeight={400}
 
-          <img src="https://images.pexels.com/photos/219095/pexels-photo-219095.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="pic"/>
+>
+<Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGAYkgD8ZK-12QjvLIjcvmb8fHjrk1KNLZVA&usqp=CAU"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGAYkgD8ZK-12QjvLIjcvmb8fHjrk1KNLZVA&usqp=CAU"
+          alt="Second slide"
+        />
 
-      </Carousel>
-</div>
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGAYkgD8ZK-12QjvLIjcvmb8fHjrk1KNLZVA&usqp=CAU"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+</Box>
   )
 }
 
